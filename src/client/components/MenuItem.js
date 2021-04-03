804891a20303c5cf1e8cf5d69364b86f45a2758c
Mike Wittenauer <mikewittenauer@me.com>
2021-04-03 01:09:18 -0400

    Update image url in menu item component

--- src/client/components/MenuItem.js
+++ src/client/components/MenuItem.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const MenuItem = ({ category, name, image }) => {
-    const imageBaseUrl = process.env.IMAGE_BASE_URL;
-
     return (
         <div className={ `col-md-3 col-sm-6 ${category}` }>       
             <div>                
                 <div className="portfolio-thumb">
-                    <img src={ `${imageBaseUrl}${image}` } alt="" />
+                    <img src={ `${image}` } alt="" />
                     <div className="hover">
                         <div className="hover-iner">
-                            <a href={ `${imageBaseUrl}${image}` }><img src="images/open-icon.png" alt="" /></a>
+                            <a href={ `${image}` }><img src="images/open-icon.png" alt="" /></a>
                             <span>{ category }</span>
                         </div>
                     </div>
