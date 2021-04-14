dfe0e23251d2b53cb056a75b97831fbc05afd78a
Mike Wittenauer <mikewittenauer@me.com>
2021-04-14 19:56:33 -0400

    Update links to auto scroll

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
+        <footer id="footer">
             <div className="container">
                 <div className="top-footer">
                 </div>
